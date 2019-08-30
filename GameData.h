ff78b70869ea2cd49339cabe60bcb111d843a1e6
leha-sys <3244055+leha-sys@users.noreply.github.com>
2019-08-30 17:42:14 +0500

    process

---
+++ GameData.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#ifndef _GAMEDATA_H
+#define _GAMEDATA_H
+
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+class Entity
+{
+public:
+	std::string name;
+	int health;
+	int damage;
+};
+
+struct GameTile
+{
+	int x, y;
+	bool isEndGameTile{false};
+	std::string RoomDesc;
+	std::vector<Entity*> EntityList;
+	GameTile *NorthTile{ NULL };
+	GameTile *SouthTile{ NULL };
+	GameTile *EastTile{ NULL };
+	GameTile *WestTile{ NULL };
+};
+
+class Player : public Entity
+{
+public:
+	GameTile * currentTile{ NULL };
+	Player(std::string n, int h, int d, GameTile *gT)
+	{
+		name = n;
+		health = h;
+		damage = d;
+		currentTile = gT;
+	}
+};
+
+class Enemy : public Entity
+{
+public:
+	bool isDead;
+	Enemy(std::string n, int h, int d, bool b)
+	{
+		name = n;
+		health = h;
+		damage = d;
+		isDead = b;
+	}
+};
+
+class Shrine : public Entity
+{
+public:
+	bool used;
+	Shrine(std::string n, int h, int d, bool b)
+	{
+		name = n;
+		health = h;
+		damage = d;
+		used = b;
+	}
+};
+
+std::vector<GameTile> GameMap3x1;
+
+//GameTile *NorthTile{ NULL };
+//GameTile *SouthTile{ NULL };
+//GameTile *EastTile{ NULL };
+//GameTile *WestTile{ NULL };
+
+enum { NODOOR, EAST, WEST, NORTH, SOUTH };
+
+#endif
