45370445b3548ee10336d6eca6c4518c09d01ecf
leha-sys <3244055+leha-sys@users.noreply.github.com>
2019-09-02 14:15:26 +0500

    four rooms and core rewrite

--- Game2.cpp
+++ Game2.cpp
@@ -1,47 +1,65 @@
 ﻿#include "GameHeaders.h"
 
+/*
+TODO:
+show health in the main room (console out)
+take health in the main room from shrine (new function)
+choise to go or to take a health
+
+*/
+
+int RoomAction(Player *player)
+{
+	for (int i = 0; i < player->currentTile->EntityList.size(); i++)
+	{
+		std::cout << "There is a " << player->currentTile->EntityList[i]->name << " in the room." << std::endl;
+		//if (player.currentTile->EntityList[i]->name == "Slime")
+		if (player->currentTile->EntityList[i]->isEnemy == true)
+		{
+			while (player->health > 0 && player->currentTile->EntityList[i]->health > 0)
+			{
+
+				std::cout << player->name << " is attacked by a " << player->currentTile->EntityList[i]->name << "." << std::endl;
+				player->health -= player->currentTile->EntityList[i]->damage;
+				std::cout << player->name << "'s health is now at: " << player->health << std::endl;
+				if (player->health < 0)
+				{
+					std::cout << "You Died..." << std::endl;
+					system("pause");
+					return 1;
+				}
+				std::cout << player->name << " attacks the " << player->currentTile->EntityList[i]->name << "." << std::endl;
+				player->currentTile->EntityList[i]->health -= player->damage;
+				std::cout << "The " << player->currentTile->EntityList[i]->name << "'s health is now at: " << player->currentTile->EntityList[i]->health << "\n" << std::endl;
+				system("pause");
+				system("cls");
+			}
+			if (player->health < 0)
+			{
+				std::cout << "You Died..." << std::endl;
+				system("pause");
+				return 1;
+			}
+			else
+			{
+				std::cout << "The " << player->currentTile->EntityList[i]->name << " is dead." << "\n" << std::endl;
+			}
+		}
+	}
+	return 0;
+}
+
 int main()
 {
 	CreateMap3x1();
 	Player player{ "Hero", 100, 5, &GameMap3x1[0] };
 
-
 	while (player.health > 0 && player.currentTile->isEndGameTile != true)
 	{
 		system("cls");
 		std::cout << player.currentTile->RoomDesc << std::endl;
-		for (int i = 0; i < player.currentTile->EntityList.size(); i++)
-		{
-			std::cout << "There is a " << player.currentTile->EntityList[i]->name << " in the room." << std::endl;
-			//if (player.currentTile->EntityList[i]->name == "Slime")
-			if (player.currentTile->EntityList[i]->isEnemy == true)
-			{
-				while (player.health > 0 && player.currentTile->EntityList[i]->health > 0)
-				{
 
-					std::cout << player.name << " is attacked by a " << player.currentTile->EntityList[i]->name << "." << std::endl;
-					player.health -= player.currentTile->EntityList[i]->damage;
-					std::cout << player.name << "'s health is now at: " << player.health << std::endl;
-					std::cout << player.name << " attacks the " << player.currentTile->EntityList[i]->name << "." << std::endl;
-					player.currentTile->EntityList[i]->health -= player.damage;
-					std::cout << "The " << player.currentTile->EntityList[i]->name << "'s health is now at: " << player.currentTile->EntityList[i]->health << "\n" << std::endl;
-					system("pause");
-					system("cls");
-				}
-				if (player.health < 0)
-				{
-					std::cout << "You Died..." << std::endl;
-					system("pause");
-					return 0;
-				}
-				else
-				{
-					std::cout << "The " << player.currentTile->EntityList[i]->name << " is dead." << "\n" << std::endl;
-				}
-
-			}
-		}
-
+		if (RoomAction(&player) == 1) return 0;
 
 		LookForDoors(&player);
 		ShowDoors();
@@ -64,3 +82,5 @@ int main()
 	system("pause");
 	return 0;
 }
+
+
--- GameMap.h
+++ GameMap.h
@@ -8,19 +8,23 @@
 
 void CreateMap3x1() {
 	//Tile 1
-	GameMap3x1.push_back(GameTile{ 0, 0, false, "A well lit room with a strange statue in the center." });
+	GameMap3x1.push_back(GameTile{ 0, 0, false, "A well lit room." });
 	GameMap3x1[0].EntityList.push_back(new Shrine("Health Shrine", 0, 0, false));
 
 	//Tile 2
-	GameMap3x1.push_back(GameTile{ 0, 0, false, "A damp vile room." });
+	GameMap3x1.push_back(GameTile{ 0, 0, false, "Dark room." });
 	GameMap3x1[1].EntityList.push_back(new Enemy("Slime", 30, 10, true));
 
 	//Tile 3
 	GameMap3x1.push_back(GameTile{ 0, 0, true, "You escape." });
 
 	//Tile 4
-	GameMap3x1.push_back(GameTile{ 0, 0, false, "Villiage" });
+	GameMap3x1.push_back(GameTile{ 0, 0, false, "Very dark room." });
 	GameMap3x1[3].EntityList.push_back(new Enemy("Monster", 10, 10, true));
+
+	//Tile 5
+	GameMap3x1.push_back(GameTile{ 0, 0, false, "Underground." });
+	GameMap3x1[4].EntityList.push_back(new Enemy("Great Monster", 30, 30, true));
 
 	GameMap3x1[0].EastTile = &GameMap3x1[1];
 	GameMap3x1[1].WestTile = &GameMap3x1[0];
@@ -28,6 +32,9 @@ void CreateMap3x1() {
 
 	GameMap3x1[0].NorthTile = &GameMap3x1[3];
 	GameMap3x1[3].SouthTile = &GameMap3x1[0];
+
+	GameMap3x1[0].SouthTile = &GameMap3x1[4];
+	GameMap3x1[4].NorthTile = &GameMap3x1[0];
 }
 
 #endif
